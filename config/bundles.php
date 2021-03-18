97489a607c4f1fb64113e391579d34b574afcefd
Arkowsky <arkadiusz.prosciak@gmail.com>
2021-03-18 19:34:14 +0100

    Add doctrine and dev database config

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,8 @@
 <?php
-declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
